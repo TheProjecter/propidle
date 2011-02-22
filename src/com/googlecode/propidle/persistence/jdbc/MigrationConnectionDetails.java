5d001e86710351788c8206c3798bd38ab6b8c872
Matthew Savage <matthew.savage@gmail.com>
2011-02-22 11:05:35 +0000

    Using same property for migration and normal use jdbc url

--- src/com/googlecode/propidle/persistence/jdbc/MigrationConnectionDetails.java
+++ src/com/googlecode/propidle/persistence/jdbc/MigrationConnectionDetails.java
@@ -5,7 +5,6 @@ import static com.googlecode.propidle.properties.Properties.getOrFail;
 import java.util.Properties;
 
 public class MigrationConnectionDetails extends ConnectionDetails {
-    public static final String URL = "migration.jdbc.url";
     public static final String USER = "migration.jdbc.user";
     public static final String PASSWORD = "migration.jdbc.password";
 
