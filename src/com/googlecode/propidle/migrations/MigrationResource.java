ca1e26c4121d2a0f27dbc29d442d789970ec5de1
Thomas Lee <thomasalanlee@gmail.com>
2011-03-10 13:31:16 +0000

    display the result of running migrations in a nice way. and today I am wearing a pink shirt

--- src/com/googlecode/propidle/migrations/MigrationResource.java
+++ src/com/googlecode/propidle/migrations/MigrationResource.java
@@ -3,6 +3,8 @@ package com.googlecode.propidle.migrations;
 import com.googlecode.propidle.migrations.log.MigrationLogItem;
 import com.googlecode.propidle.server.PropertiesModule;
 import com.googlecode.propidle.server.RunMigrations;
+import com.googlecode.propidle.status.StatusResource;
+import com.googlecode.totallylazy.Callable1;
 import com.googlecode.totallylazy.Callable2;
 import com.googlecode.totallylazy.Sequence;
 import com.googlecode.utterlyidle.rendering.Model;
@@ -16,18 +18,20 @@ import java.util.Properties;
 import static com.googlecode.propidle.server.PropertiesApplication.inTransaction;
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.totallylazy.callables.TimeCallable.calculateMilliseconds;
+import static com.googlecode.utterlyidle.proxy.Resource.resource;
+import static com.googlecode.utterlyidle.proxy.Resource.urlOf;
 import static com.googlecode.utterlyidle.rendering.Model.model;
 import static java.lang.String.format;
 import static java.lang.System.nanoTime;
 import static javax.ws.rs.core.MediaType.TEXT_HTML;
 
-@Path(MigrationsResource.NAME)
+@Path(MigrationResource.NAME)
 @Produces(TEXT_HTML)
-public class MigrationsResource {
+public class MigrationResource {
     private final Properties properties;
     public static final String NAME = "migrations";
 
-    public MigrationsResource(Properties properties) {
+    public MigrationResource(Properties properties) {
         this.properties = properties;
     }
 
@@ -42,18 +46,17 @@ public class MigrationsResource {
         } finally {
             container.close();
         }
-        Model model = model()
-                .add("Run time", format("%s ms", calculateMilliseconds(start, nanoTime())))
+        return model()
+                .add("runTime", format("%s ms", calculateMilliseconds(start, nanoTime())))
+                .add("statusReport", urlOf(resource(StatusResource.class).reportStatus()))
+                .add("migrations", migrations.map(reportMigration()))
                 .add(PropertiesModule.MODEL_NAME, NAME);
-
-        return migrations.fold(model, reportMigrations());
     }
 
-    private Callable2<Model, MigrationLogItem, Model> reportMigrations() {
-        return new Callable2<Model, MigrationLogItem, Model>() {
-            public Model call(Model model, MigrationLogItem item) throws Exception {
-                model.add(format("Migration %s", item.number()), format("ran at %s", item.dateRun()));
-                return model;
+    private Callable1<MigrationLogItem, String> reportMigration() {
+        return new Callable1<MigrationLogItem, String>() {
+            public String call(MigrationLogItem migrationLogItem) throws Exception {
+                return format("%s-%s.sql", migrationLogItem.number(), migrationLogItem.name());
             }
         };
     }
