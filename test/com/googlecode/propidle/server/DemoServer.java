fd4cf58e715c6779f9066870e9b406887b73a9ca
Thomas Lee <thomasalanlee@gmail.com>
2011-04-19 10:46:41 +0100

    upgrade utterlyidle

--- test/com/googlecode/propidle/server/DemoServer.java
+++ test/com/googlecode/propidle/server/DemoServer.java
@@ -1,16 +1,15 @@
 package com.googlecode.propidle.server;
 
 import com.googlecode.propidle.TestPropertiesApplication;
+import com.googlecode.utterlyidle.jetty.RestApplicationActivator;
 import com.googlecode.utterlyidle.simpleframework.RestServer;
 
 import static com.googlecode.utterlyidle.BasePath.basePath;
 
 public class DemoServer {
     public static void main(String[] args) throws Exception {
-        TestPropertiesApplication application = new TestPropertiesApplication();
-
         int port = args.length > 0 ? Integer.valueOf(args[0]) : 8000;
-        new RestServer(port, basePath("/"), application);
+        new RestServer(port, basePath("/"), new RestApplicationActivator(new TestPropertiesApplication()));
     }
 
 }
