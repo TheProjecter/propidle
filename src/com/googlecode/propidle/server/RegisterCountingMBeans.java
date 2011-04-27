6bcd842e6cce0bb9da70b973fa599fc1b6947eda
Thomas Lee <thomasalanlee@gmail.com>
2011-04-27 16:22:20 +0100

    adding initial stab at caching; unregister mbeans when stopping the server

--- src/com/googlecode/propidle/server/RegisterCountingMBeans.java
+++ src/com/googlecode/propidle/server/RegisterCountingMBeans.java
@@ -2,23 +2,35 @@ package com.googlecode.propidle.server;
 
 import com.googlecode.propidle.monitoring.HttpRequestCounter;
 
-import javax.management.MBeanServer;
-import javax.management.ObjectName;
+import javax.management.*;
+import java.io.Closeable;
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.util.concurrent.Callable;
 
-public class RegisterCountingMBeans implements Callable<Void> {
+public class RegisterCountingMBeans implements Callable<Void>, Closeable {
     private final HttpRequestCounter httpRequestCounter;
+    private ObjectName mBeanName;
 
-    public RegisterCountingMBeans(HttpRequestCounter httpRequestCounter) {
+    public RegisterCountingMBeans(HttpRequestCounter httpRequestCounter) throws MalformedObjectNameException {
         this.httpRequestCounter = httpRequestCounter;
+        mBeanName = new ObjectName("propidle.monitoring:type=HttpRequestCounter");
     }
 
     public Void call() throws Exception {
         MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
-        ObjectName mBeanName = new ObjectName("propidle.monitoring:type=HttpRequestCounter");
         mBeanServer.registerMBean(httpRequestCounter, mBeanName);
-
         return null;
+    }
+
+    public void close() throws IOException {
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        try {
+            mBeanServer.unregisterMBean(mBeanName);
+        } catch (InstanceNotFoundException e) {
+            throw new IOException(e);
+        } catch (MBeanRegistrationException e) {
+            throw new IOException(e);
+        }
     }
 }
