5619eb28340d9fc63e8936dbaee13711a47c6852
Thomas Lee <thomasalanlee@gmail.com>
2011-08-01 11:02:49 +0100

    don't try to resolve properties that are not in use

--- src/com/googlecode/propidle/client/loaders/ResolveVariables.java
+++ src/com/googlecode/propidle/client/loaders/ResolveVariables.java
@@ -1,61 +1,66 @@
 package com.googlecode.propidle.client.loaders;
 
-import com.googlecode.totallylazy.Callable2;
-import com.googlecode.totallylazy.Pair;
+import com.googlecode.totallylazy.*;
 
 import java.util.Properties;
 import java.util.concurrent.Callable;
 
 import static com.googlecode.propidle.properties.Properties.toPairs;
+import static com.googlecode.totallylazy.Sequences.sequence;
 
 public final class ResolveVariables implements Callable<Properties> {
     private final Callable<Properties> loader;
-    private final String[] propertyNames;
+    private final Sequence<String> propertyNames;
 
-    public static ResolveVariables resolveProperties(Callable<Properties> loader, String ... propertyNames) {
-        return new ResolveVariables(loader, propertyNames);
+    public static ResolveVariables resolveProperties(Callable<Properties> loader, String propertyName, String... additionalPropertyNames) {
+        return new ResolveVariables(loader, sequence(propertyName).join(sequence(additionalPropertyNames)));
     }
 
-    private ResolveVariables(Callable<Properties> loader, String ... propertyNames) {
-        checkPropertyNamesExist(propertyNames);
-
+    private ResolveVariables(Callable<Properties> loader, Sequence<String> propertyNames) {
         this.loader = loader;
         this.propertyNames = propertyNames;
     }
 
     public Properties call() throws Exception {
         Properties properties = loader.call();
-        return toPairs(properties).fold(properties, resolveVariables(propertyNames));
+        return propertyNames.fold(properties, resolvePropertyName());
     }
 
-    private static Callable2<? super Properties, ? super Pair<String, String>, Properties> resolveVariables(final String ... propertyNames) {
-        return new Callable2<Properties, Pair<String, String>, Properties>() {
-            public Properties call(Properties properties, Pair<String, String> property) throws Exception {
-                for (String propertyName : propertyNames) {
-                    checkThatPropertyExists(properties, propertyName);
+    private static Callable2<? super Properties, ? super String, Properties> resolvePropertyName() {
+        return new Callable2<Properties, String, Properties>() {
+            public Properties call(Properties properties, String propertyName) throws Exception {
+                if(propertyIsUsed(properties, resolvablePropertyName(propertyName))) {
+                    checkThatPropertyIsDefined(properties, propertyName);
+                    return toPairs(properties).fold(properties, resolve(propertyName));
+                }
 
-                    if(property.second().contains(resolvablePropertyName(propertyName))){
-                        properties.setProperty(property.first(), resolveProperty(properties, property.second(), propertyName));
-                    }
-                }   
                 return properties;
             }
         };
+    }
+
+    private static Callable2<? super Properties, ? super Pair<String, String>, Properties> resolve(final String propertyName) {
+        return new Callable2<Properties, Pair<String, String>, Properties>() {
+            public Properties call(Properties properties, Pair<String, String> property) throws Exception {
+                if(property.second().contains(resolvablePropertyName(propertyName))){
+                    properties.setProperty(property.first(), resolveProperty(properties, property.second(), propertyName));
+                }
+                return properties;
+            }
+        };
+    }
+
+    private static boolean propertyIsUsed(Properties properties, final String propertyName) {
+        return toPairs(properties).map(Callables.<String>second()).exists(Strings.contains(propertyName));
     }
 
     private static String resolvablePropertyName(String propertyName) {
         return String.format("${%s}", propertyName);
     }
 
-    private static void checkThatPropertyExists(Properties properties, String propertyName) {
+    private static void checkThatPropertyIsDefined(Properties properties, String propertyName) {
         if (!containsProperty(properties, propertyName)) {
             throw new IllegalArgumentException(String.format("Property %s not found in %s", propertyName, properties));
-        }
-    }
-
-    private static void checkPropertyNamesExist(String[] propertyNames) {
-        if (propertyNames == null || propertyNames.length == 0) {
-            throw new IllegalArgumentException("No property names were specified.");
         }
     }
 
