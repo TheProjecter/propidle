7d2df5a700c40146ac6961052f8da6f6bd854845
Laszlo Kiss <pityq1@gmail.com>
2011-08-16 17:01:41 +0100

    renders template around  exception messages

---
+++ src/com/googlecode/propidle/exceptions/ExceptionFormattingModule.java
@@ -0,0 +1,53 @@
+package com.googlecode.propidle.exceptions;
+
+import com.googlecode.propidle.server.ModelTemplateRenderer;
+import com.googlecode.totallylazy.Pair;
+import com.googlecode.totallylazy.Predicates;
+import com.googlecode.totallylazy.predicates.LogicalPredicate;
+import com.googlecode.utterlyidle.*;
+import com.googlecode.utterlyidle.handlers.ExceptionHandler;
+import com.googlecode.utterlyidle.handlers.ResponseHandlers;
+import com.googlecode.utterlyidle.modules.Module;
+import com.googlecode.utterlyidle.modules.RequestScopedModule;
+import com.googlecode.utterlyidle.modules.ResponseHandlersModule;
+import com.googlecode.utterlyidle.rendering.ExceptionRenderer;
+import com.googlecode.yadic.Container;
+
+import static com.googlecode.propidle.server.decoration.Decorators.acceptsHtml;
+import static com.googlecode.propidle.server.decoration.Decorators.returnsHtml;
+import static com.googlecode.totallylazy.Pair.pair;
+import static com.googlecode.totallylazy.Predicates.*;
+import static com.googlecode.utterlyidle.HeaderParameters.headerParameters;
+import static com.googlecode.utterlyidle.HttpHeaders.CONTENT_TYPE;
+import static com.googlecode.utterlyidle.MediaType.TEXT_HTML;
+import static com.googlecode.utterlyidle.Status.INTERNAL_SERVER_ERROR;
+import static com.googlecode.utterlyidle.handlers.HandlerRule.entity;
+import static com.googlecode.utterlyidle.handlers.RenderingResponseHandler.renderer;
+import static com.googlecode.utterlyidle.rendering.Model.model;
+
+
+public class ExceptionFormattingModule implements RequestScopedModule, ResponseHandlersModule {
+    public Module addPerRequestObjects(Container container) throws Exception {
+        container.decorate(HttpHandler.class, ExceptionHandler.class);
+        return this;
+    }
+
+    public Module addResponseHandlers(ResponseHandlers handlers) throws Exception {
+        LogicalPredicate<Pair<Request,Response>> where = Predicates.<Pair<Request, Response>, Object>where(entity(), is(instanceOf(Exception.class))).and(acceptsHtml());
+        handlers.add(where, exceptionResponseHandler());
+        return this;
+    }
+
+    private ResponseHandler exceptionResponseHandler() {
+        return new ResponseHandler() {
+            public Response handle(Response response) throws Exception {
+                Response htmlResponse = Responses.response(INTERNAL_SERVER_ERROR,
+                        headerParameters(pair(CONTENT_TYPE, TEXT_HTML)),
+                        model().
+                                add("errorMessage", "An error occured in propidle").
+                                add("stackTrace", ExceptionRenderer.toString((Exception) response.entity())));
+                return renderer(new ModelTemplateRenderer("Error_html", ExceptionFormattingModule.class)).handle(htmlResponse);
+            }
+        };
+    }
+}
