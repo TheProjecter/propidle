76b00157d8d6898766abe25fa3f1c00af5437b81
laszlo <laszlo@localhost>
2012-03-14 18:01:20 +0000

    increase property value column size

---
+++ src/com/googlecode/propidle/migrations/hsql/004-modify_value_column_size.sql
@@ -0,0 +1,2 @@
+alter table changes alter column previous_value VARCHAR(4000);
+alter table changes alter column updated_value VARCHAR(4000);
---
+++ src/com/googlecode/propidle/migrations/oracle/004-modify_value_column_size.sql
@@ -0,0 +1,4 @@
+alter table changes modify (
+    previous_value         VARCHAR(4000),
+    updated_value          VARCHAR(4000)
+);
