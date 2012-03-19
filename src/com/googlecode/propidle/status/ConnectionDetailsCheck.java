ad0fb387790e1816e328fbbd3ceb0b475f9bd011
laszlo <laszlo@localhost>
2012-03-19 11:52:33 +0000

    status page should return 200 even when migration required

--- src/com/googlecode/propidle/status/ConnectionDetailsCheck.java
+++ src/com/googlecode/propidle/status/ConnectionDetailsCheck.java
@@ -20,15 +20,11 @@ public class ConnectionDetailsCheck implements StatusCheck {
     }
 
     public StatusCheckResult check() throws Exception {
-        boolean canconnect = canConnect();
-        StatusCheckResult result = statusCheckResult(
+        return statusCheckResult(
                 statusCheckName(getClass().getSimpleName())).
                 add("JDBC URL", connectionDetails.url()).
                 add("Username", connectionDetails.user()).
-                add("Result", (canconnect ? "PASS" : "FAIL"));
-        result.setFatal(!canconnect);
-
-        return result;
+                add("Result", (canConnect() ? "PASS" : "FAIL"));
     }
 
     private boolean canConnect() throws Exception {
