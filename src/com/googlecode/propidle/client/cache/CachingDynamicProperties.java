ede384fa04f5b3add82c477d44b0cabf4cee6fd6
laszlo,ali <laszlo,ali@localhost>
2012-07-27 16:53:18 +0100

    add posibility to log error message when we use cache

--- src/com/googlecode/propidle/client/cache/CachingDynamicProperties.java
+++ src/com/googlecode/propidle/client/cache/CachingDynamicProperties.java
@@ -1,20 +1,25 @@
 package com.googlecode.propidle.client.cache;
 
+import com.googlecode.propidle.client.logging.Logger;
+import com.googlecode.propidle.util.Exceptions;
 import com.googlecode.totallylazy.Callable1;
 
 import java.io.*;
 import java.util.Properties;
 import java.util.concurrent.Callable;
 
+import static com.googlecode.propidle.client.logging.Message.message;
+import static com.googlecode.propidle.client.logging.PrintStreamLogger.printStreamLogger;
 import static com.googlecode.totallylazy.Closeables.using;
 
 public class CachingDynamicProperties {
-    public static Callable<Properties> caching(final File cache, final Callable<Properties> callable) {
+    public static Callable<Properties> caching(final File cache, final Callable<Properties> callable, final Logger logger) {
         return new Callable<Properties>() {
             public Properties call() throws Exception {
                 try {
                     return toCache(cache,callable.call());
                 } catch (Exception e) {
+                    logger.log(message(Exceptions.stackTraceToString(e)));
                     return fromCache(cache);
                 }
             }
@@ -28,6 +33,10 @@ public class CachingDynamicProperties {
                 });
             }
         };
+    }
+
+    public static Callable<Properties> caching(final File cache, final Callable<Properties> callable) {
+        return caching(cache, callable, printStreamLogger(System.err));
     }
 
     private static Properties fromCache(File cache) throws FileNotFoundException {
--- src/com/googlecode/propidle/util/Exceptions.java
+++ src/com/googlecode/propidle/util/Exceptions.java
@@ -1,8 +1,17 @@
 package com.googlecode.propidle.util;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 public class Exceptions {
     public static Exception toException(Throwable throwable) throws Exception {
         if(throwable instanceof Error) throw (Error) throwable;
         return (Exception) throwable;
+    }
+
+    public static String stackTraceToString(Exception e) {
+        StringWriter stackTrace = new StringWriter();
+        e.printStackTrace(new PrintWriter(stackTrace));
+        return stackTrace.toString();
     }
 }
