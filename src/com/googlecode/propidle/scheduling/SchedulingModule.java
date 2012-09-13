7fc21e17c0eddb9160d1b8f76d5913935a0ed855
stuart & raymond <stuart & raymond@localhost>
2012-09-13 16:29:23 +0100

    Close ScheduledExecutorService on close

--- src/com/googlecode/propidle/scheduling/SchedulingModule.java
+++ src/com/googlecode/propidle/scheduling/SchedulingModule.java
@@ -1,9 +1,11 @@
 package com.googlecode.propidle.scheduling;
 
+import com.googlecode.utterlyidle.CloseableCallable;
 import com.googlecode.utterlyidle.Resources;
 import com.googlecode.utterlyidle.modules.*;
 import com.googlecode.yadic.Container;
 
+import java.io.IOException;
 import java.util.concurrent.ScheduledExecutorService;
 
 import static com.googlecode.utterlyidle.annotations.AnnotatedBindings.annotatedClass;
@@ -22,9 +24,25 @@ public class SchedulingModule implements ResourcesModule, ModuleDefiner, Applica
 
     public Module addPerApplicationObjects(Container container) {
         container.add(ScheduleTask.class, ScheduleTaskRequest.class);
-        container.addInstance(ScheduledExecutorService.class, newScheduledThreadPool(5));
+        container.addActivator(ScheduledExecutorService.class, closeableExecutorService());
         container.add(SchedulableRequests.class);
         container.add(Scheduler.class);
         return this;
+    }
+
+    private CloseableCallable<ScheduledExecutorService> closeableExecutorService() {
+        return new CloseableCallable<ScheduledExecutorService>() {
+            private final ScheduledExecutorService scheduledExecutorService = newScheduledThreadPool(5);
+
+            @Override
+            public ScheduledExecutorService call() throws Exception {
+                return scheduledExecutorService;
+            }
+
+            @Override
+            public void close() throws IOException {
+                scheduledExecutorService.shutdownNow();
+            }
+        };
     }
 }
